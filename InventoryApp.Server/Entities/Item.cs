b9ec70696cd1bb5c61fc9e7e9382e33e94a04d6b
akhun <azizkhon_akhunov@student.itpu.uz>
2026-02-16 15:03:42 +0500

    Entity Item created

---
+++ InventoryApp.Server/Entities/Item.cs
@@ -0,0 +1,34 @@
+﻿namespace InventoryApp.Server.Entities
+{
+    public class Item : BaseEntity
+    {
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; } = null!;
+
+        public string CustomId { get; set; } = null!;
+
+        public Guid CreatedById { get; set; }
+        public User CreatedBy { get; set; } = null!;
+
+        public int Version { get; set; }
+        public string? Text1 { get; set; }
+        public string? Text2 { get; set; }
+        public string? Text3 { get; set; }
+
+        public string? MultiText1 { get; set; }
+        public string? MultiText2 { get; set; }
+        public string? MultiText3 { get; set; }
+
+        public decimal? Number1 { get; set; }
+        public decimal? Number2 { get; set; }
+        public decimal? Number3 { get; set; }
+
+        public bool? Bool1 { get; set; }
+        public bool? Bool2 { get; set; }
+        public bool? Bool3 { get; set; }
+
+        public string? Doc1 { get; set; }
+        public string? Doc2 { get; set; }
+        public string? Doc3 { get; set; }
+    }
+}
