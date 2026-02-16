d369515d2593150075500b0f23504d53957bfa54
akhun <azizkhon_akhunov@student.itpu.uz>
2026-02-16 15:10:58 +0500

    InventoryAccess Entity added

---
+++ InventoryApp.Server/Entities/InventoryAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryApp.Server.Entities
+{
+    public class InventoryAccess : BaseEntity
+    {
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
