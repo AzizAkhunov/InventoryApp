dca5c2c52133ccfe63bb496b3e9842e9f39d59ea
akhun <azizkhon_akhunov@student.itpu.uz>
2026-02-24 14:40:38 +0500

    ItemDTO created

---
+++ InventoryApp.Application/DTO/ItemDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace InventoryApp.Application.DTO
+{
+    public class ItemDTO
+    {
+        public Guid Id { get; set; }
+
+        public Guid InventoryId { get; set; }
+
+        public string CustomId { get; set; } = null!;
+
+        public int Version { get; set; }
+
+        public string? Text1 { get; set; }
+        public string? Text2 { get; set; }
+        public string? Text3 { get; set; }
+
+        public string? MultiText1 { get; set; }
+        public string? MultiText2 { get; set; }
+        public string? MultiText3 { get; set; }
+
+        public decimal? Number1 { get; set; }
+        public decimal? Number2 { get; set; }
+        public decimal? Number3 { get; set; }
+
+        public bool? Bool1 { get; set; }
+        public bool? Bool2 { get; set; }
+        public bool? Bool3 { get; set; }
+
+        public string? Doc1 { get; set; }
+        public string? Doc2 { get; set; }
+        public string? Doc3 { get; set; }
+    }
+}
