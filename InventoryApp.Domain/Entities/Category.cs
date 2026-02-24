ead88511d7232b4a2eff4e3038f3b3e45adf62e2
akhun <azizkhon_akhunov@student.itpu.uz>
2026-02-24 12:37:45 +0500

    Category Entity added , Mugration changed

---
+++ InventoryApp.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryApp.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        public ICollection<Inventory> Inventories { get; set; } = new List<Inventory>();
+    }
+}
