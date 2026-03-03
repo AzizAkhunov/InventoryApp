c77ef25eb1880ce7b0535bfd70c7c194b2a26542
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-03 14:51:13 +0500

    ItemLike table created and added

---
+++ InventoryApp.Domain/Entities/ItemLike.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryApp.Domain.Entities
+{
+    public class ItemLike
+    {
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
--- InventoryApp.Infrastructure/Data/AppDbContext.cs
+++ InventoryApp.Infrastructure/Data/AppDbContext.cs
@@ -190,6 +190,22 @@ namespace InventoryApp.Infrastructure.Data
                 .HasForeignKey(p => p.AuthorId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+
+            //ITEMLIKE 
+            modelBuilder.Entity<ItemLike>()
+                .HasKey(l => new { l.ItemId, l.UserId });
+
+            modelBuilder.Entity<ItemLike>()
+                .HasOne(l => l.Item)
+                .WithMany()
+                .HasForeignKey(l => l.ItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<ItemLike>()
+                .HasOne(l => l.User)
+                .WithMany()
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
