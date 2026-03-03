60fb400bd35658306ee99ee995ef3ddd6845c821
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-03 15:07:52 +0500

    JwtService added

---
+++ InventoryApp.Application/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using InventoryApp.Domain.Entities;
+
+namespace InventoryApp.Application.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);    
+    }
+}
---
+++ InventoryApp.Application/Services/JwtService.cs
@@ -0,0 +1,34 @@
+﻿using InventoryApp.Application.Interfaces;
+using InventoryApp.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace InventoryApp.Application.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly string _key = "SUPER_SECRET_KEY_123456";
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User")
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
