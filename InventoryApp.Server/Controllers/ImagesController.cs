7e99519ec48987b0dd9354613e09b833d80fb8b6
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-04 20:26:11 +0500

    Cloudinary controller completed

---
+++ InventoryApp.Server/Controllers/ImagesController.cs
@@ -0,0 +1,29 @@
+﻿using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File not provided");
+
+            var url = await _imageService.UploadImageAsync(file);
+
+            return Ok(new { url });
+        }
+    }
+}
