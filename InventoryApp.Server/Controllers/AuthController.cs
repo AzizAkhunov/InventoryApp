2fefcf42f096556c9f8cf7db81a085c732f02d78
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-10 23:17:46 +0500

    backup controller

--- InventoryApp.Server/Controllers/AuthController.cs
+++ InventoryApp.Server/Controllers/AuthController.cs
@@ -3,10 +3,8 @@ using InventoryApp.Application.DTO;
 using InventoryApp.Application.Interfaces;
 using InventoryApp.Domain.Entities;
 using InventoryApp.Infrastructure.Data;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 namespace InventoryApp.Server.Controllers
 {
@@ -53,8 +51,7 @@ namespace InventoryApp.Server.Controllers
             }
 
             var email = payload.Email;
-            var googleName = payload.Name;
-            var picture = payload.Picture;
+            var name = payload.Name;
 
             var user = await _context.Users
                 .FirstOrDefaultAsync(u => u.Email == email);
@@ -65,7 +62,7 @@ namespace InventoryApp.Server.Controllers
                 {
                     Id = Guid.NewGuid(),
                     Email = email,
-                    UserName = googleName,
+                    UserName = name,
                     CreatedAt = DateTime.UtcNow
                 };
 
@@ -84,45 +81,9 @@ namespace InventoryApp.Server.Controllers
                 user = new
                 {
                     id = user.Id,
-                    userName = user.UserName,
+                    name = user.UserName,
                     email = user.Email,
-                    picture = picture,
-                    isAdmin = user.IsAdmin,
-                    isBlocked = user.IsBlocked,
-                    createdAt = user.CreatedAt
-                }
-            });
-        }
-
-
-        [Authorize]
-        [HttpPut("username")]
-        public async Task<IActionResult> UpdateUserName([FromBody] UpdateUserNameDto dto)
-        {
-            var userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
-
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
-
-            if (user == null)
-                return NotFound();
-
-            user.UserName = dto.UserName;
-
-            await _context.SaveChangesAsync();
-
-            var jwt = _jwtService.GenerateToken(user);
-
-            return Ok(new
-            {
-                token = jwt,
-                user = new
-                {
-                    id = user.Id,
-                    userName = user.UserName,
-                    email = user.Email,
-                    isAdmin = user.IsAdmin,
-                    isBlocked = user.IsBlocked,
-                    createdAt = user.CreatedAt
+                    isAdmin = user.IsAdmin
                 }
             });
         }
