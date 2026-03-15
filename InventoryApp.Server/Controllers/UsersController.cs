42ab40be1deedff2346eaeeb98a672ac41c65284
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-16 03:02:17 +0500

    Search user added

---
+++ InventoryApp.Server/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using InventoryApp.Application.DTO;
+using InventoryApp.Application.Extensions;
+using InventoryApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                return Ok(new List<UserSearchDto>());
+
+            var currentUserId = User.GetUserId();
+
+            var users = await _context.Users
+                .Where(u => u.Id != currentUserId)
+                .Where(u => u.Email.ToLower().Contains(query.ToLower()))
+                .OrderBy(u => u.Email)
+                .Select(u => new UserSearchDto
+                {
+                    Id = u.Id,
+                    Email = u.Email
+                })
+                .Take(10)
+                .ToListAsync();
+
+            return Ok(users);
+        }
+    }
+}
