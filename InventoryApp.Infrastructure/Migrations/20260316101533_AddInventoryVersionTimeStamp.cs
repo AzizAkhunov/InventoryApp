592be343193a061d861bd5a9078de40fb1284127
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-16 15:15:52 +0500

    Fix added migration

---
+++ InventoryApp.Infrastructure/Migrations/20260316101533_AddInventoryVersionTimeStamp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInventoryVersionTimeStamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Version",
+                table: "Inventories",
+                type: "bytea",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 15, 32, 568, DateTimeKind.Utc).AddTicks(1497));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 15, 32, 568, DateTimeKind.Utc).AddTicks(1506));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 15, 32, 568, DateTimeKind.Utc).AddTicks(1507));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 15, 32, 568, DateTimeKind.Utc).AddTicks(1508));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Version",
+                table: "Inventories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldRowVersion: true,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 21, 19, 0, 242, DateTimeKind.Utc).AddTicks(8337));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 21, 19, 0, 242, DateTimeKind.Utc).AddTicks(8342));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 21, 19, 0, 242, DateTimeKind.Utc).AddTicks(8343));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 21, 19, 0, 242, DateTimeKind.Utc).AddTicks(8344));
+        }
+    }
+}
