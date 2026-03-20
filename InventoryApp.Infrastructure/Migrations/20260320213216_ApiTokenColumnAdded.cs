4c64048ec2dc4e16b9ffa8149f5c7c27923e2ff4
akhun <azizkhon_akhunov@student.itpu.uz>
2026-03-21 02:32:38 +0500

    add migration

---
+++ InventoryApp.Infrastructure/Migrations/20260320213216_ApiTokenColumnAdded.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiTokenColumnAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiToken",
+                table: "Inventories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 20, 21, 32, 15, 404, DateTimeKind.Utc).AddTicks(1755));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 20, 21, 32, 15, 404, DateTimeKind.Utc).AddTicks(1765));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 20, 21, 32, 15, 404, DateTimeKind.Utc).AddTicks(1767));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 20, 21, 32, 15, 404, DateTimeKind.Utc).AddTicks(1769));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiToken",
+                table: "Inventories");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 39, 10, 113, DateTimeKind.Utc).AddTicks(8422));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 39, 10, 113, DateTimeKind.Utc).AddTicks(8427));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 39, 10, 113, DateTimeKind.Utc).AddTicks(8428));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 16, 10, 39, 10, 113, DateTimeKind.Utc).AddTicks(8429));
+        }
+    }
+}
